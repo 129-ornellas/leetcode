ae0082c5f249de313759577179e347e34cc4a4f8
129-ornellas <129.ornellas@gmail.com>
2023-01-12 17:41:59 -0300

    corrige1

--- exercicio1.js
+++ exercicio1.js
@@ -1,42 +1,19 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        validador = False
-        if ('(' in s):
-            if ')' in s:
-                for fecha in list(s):
-                    if s.index(')') > s.index('('):
-                        validador = True
-                    if s.index(')') < s.index('('):
-                        validador = False
-
-        if ('[' in s):
-            if (']' in s):
-                for fecha in list(s):
-                    if s.index(']') > s.index('['):
-                        validador = True
-                    if s.index(']') < s.index('['):
-                        validador = False
-
-        if ('{' in s):
-            if ('}' in s):
-                for fecha in list(s):
-                    if s.index('}') > s.index('{'):
-                        validador = True
-                    if s.index('}') < s.index('{'):
-                        validador = False
-        return validador
-a = Solution()
-print(a.isValid('([)]'))
-
-def isValid(s):
-    validas = {
-        "(":")",
-        "{":"}",
-        "[":"]"
-    }
-    lista = []
-    for entrada in list(s):
-        if s.index(entrada) == validas[lista[len(lista)]]:
-            lista.append(entrada)
-        else:
-            lista.append(entrada.index)
+var isValid = function(s) {
+    let entradasValidas = {
+         "(" : ")",
+         "{" : "}",
+         "[" : "]"
+     };
+     let listaDeEntradas = new Array();
+     for(let entrada = 0; entrada <s.length; entrada++){
+ 
+         if(s.charAt(entrada) === entradasValidas[listaDeEntradas[listaDeEntradas.length-1]]){
+             listaDeEntradas.pop();
+         }
+         else{
+             listaDeEntradas.push(s.charAt(entrada));
+         }
+     }
+     return (listaDeEntradas.length == 0);
+ };
+ 
